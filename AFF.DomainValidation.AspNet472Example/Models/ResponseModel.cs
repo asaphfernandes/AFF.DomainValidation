30c840c758cae7e39513f2c3866056bbe9009d58
Asaph Fernandes <asaphfernandes@hotmail.com>
2019-09-30 21:51:52 -0300

    run project and start validation

--- AFF.DomainValidation.AspNet472Example/Models/ResponseModel.cs
+++ AFF.DomainValidation.AspNet472Example/Models/ResponseModel.cs
@@ -4,7 +4,7 @@ namespace AFF.DomainValidation.AspNet472Example.Models
 {
     public class ResponseModel<TModel>
     {
-        public ValidationResponse Validation { get; set; }
+        public ValidationResponse Validation { get; set; } = new ValidationResponse();
         public TModel Model { get; set; }
     }
 }
--- AFF.DomainValidation.AspNet472Example/Properties/AssemblyInfo.cs
+++ AFF.DomainValidation.AspNet472Example/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("285f3ba8-0044-4bf1-834f-a268d83f4415")]
+[assembly: Guid("342f2074-a1af-4334-b6b6-407c00141cff")]
 
 // Version information for an assembly consists of the following four values:
 //
